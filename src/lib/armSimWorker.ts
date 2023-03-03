8f17fc54fbfe150c293ed702a451aed64ab0e7c8
Alex Kitt <alex.kitt@outlook.com>
2023-03-02 22:06:29 -0600

    Control flow featuring mov

--- src/lib/armSimWorker.ts
+++ src/lib/armSimWorker.ts
@@ -4,7 +4,29 @@ class ArmSimWorker extends SimWorker
 {
     private static override execute(instruction: number)
     {
-        console.log("now it's really instructin' time")
+        /*
+        Different instructions have their opcodes in different places, so we need to switch on the instruction format
+        before checking the opcode
+        */
+        switch ((instruction & 0xe000) >> 13)
+        {
+            case 0b000:
+                // Format 1 (move shifted register) or format 2 (add/subtract)
+                break;
+            case 0b001:
+                // Format 3 (move/compare/add/subtract immediate)
+                switch ((instruction & 0x1800) >> 11) {
+                    case 0b00:
+                        this.executeMov(instruction);
+                        break;
+                }
+                break;
+        }
+    }
+
+    private static executeMov(instruction: number)
+    {
+        console.log("it's movin' time!")
     }
 }
 
