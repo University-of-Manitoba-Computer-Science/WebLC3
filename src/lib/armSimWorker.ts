cbca2c8c366aea392e2d315a11f739d721c02cfb
Alex Kitt <alex.kitt@outlook.com>
2023-04-02 22:03:22 -0500

    Refactor armSimWorker for my cool new testing setup

--- src/lib/armSimWorker.ts
+++ src/lib/armSimWorker.ts
@@ -4,42 +4,46 @@ class ArmSimWorker extends SimWorker
 {
     protected static override execute(instruction: number)
     {
+        console.log(instruction.toString(16))
+
         /*
-        Different instructions have their opcodes in different places, so we need to switch on the instruction format
-        before checking the opcode
+        Different instructions have their opcodes in different places, so we need to check the instruction format before
+        checking the opcode
         */
-        switch ((instruction & 0xe000) >> 13)
-        {
-            case 0b000:
-                // Format 1 (move shifted register) or format 2 (add/subtract)
-                break;
-            case 0b001:
-                // Format 3 (move/compare/add/subtract immediate)
-                switch ((instruction & 0x1800) >> 11) {
-                    case 0b00:
-                        // MOV not implemented since there's no LC-3 equivalent
-                        break;
-                    case 0b10:
-                        this.executeAdd(instruction);
-                        break;
-                }
-                break;
-            case 0b110:
-                this.executeSwi(instruction);
-                break;
-        }
+        if ((instruction & 0xe000) >> 13 == 0b001)
+            this.executeFormat3(instruction);
+        else if ((instruction & 0xff00) >> 8 == 0b11011111)
+            this.executeSwi(instruction);
     }
 
+    /**
+     * Parses an instruction in format 3 (move/compare/add/subtract immediate) and calls the appropriate execute
+     * function
+     * @param {number} instruction
+     */
+    private static executeFormat3(instruction: number)
+    {
+        console.log("format 3")
+
+        this.executeAdd(instruction)
+    }
+
+    // Executes an add instruction
     private static executeAdd(instruction: number)
     {
+        console.log("add")
+
         const registerNumber = (instruction & 0x0700) >> 8;
         const value = instruction & 0x00ff;
 
         this.setRegister(registerNumber, this.getRegister(registerNumber) + value);
     }
 
+    // Executes an swi instruction
     private static executeSwi(instruction: number)
     {
+        console.log("swi")
+
         const value = instruction & 0x00ff;
 
         console.log(value);
