d2516b9bc0fe7e4113f500eb05cab9b5e8c5f12b
Alex Kitt <alex.kitt@outlook.com>
2023-04-14 19:29:28 -0500

    Add .blkw

--- src/logic/assembler/armParser.ts
+++ src/logic/assembler/armParser.ts
@@ -1209,9 +1209,40 @@ export default class ArmParser extends Parser
     : number
     {
         let increment = 0;
-
+        let value;
         switch (tokens[0])
         {
+            case ".blkw":
+                const amount = this.parseImmediate(tokens[1], false, lineNum);
+                value = 0;
+                if (tokens.length === 3)
+                {
+                    /*
+                    If the first character is alphabetical or an underscore, treat it as a label; otherwise, treat it as
+                    an immediate value
+                    */
+                    if (tokens[2].match(/^[a-zA-Z_]/) != null
+                        && tokens[2].match(/^[xX][0-9a-fA-F]+$/) == null
+                        && tokens[2].match(/^[bB][0-1]+$/) == null
+                    )
+                    {
+                        increment = 1;
+                        toFix.set(tokens, pc);
+                    }
+                    else
+                    {
+                        value = this.parseImmediate(tokens[2], false, lineNum);
+                    }
+                }
+                if (!isNaN(value) && !isNaN(amount))
+                {
+                    for (let i = 0; i < amount; i++)
+                    {
+                        memory[pc++] = value;
+                    }
+                    increment = amount;
+                }
+                break;
             case ".stringz":
                 const asciiCodes = this.stringToCodes(tokens[1], lineNum);
                 if (asciiCodes !== null && asciiCodes.length > 0)
