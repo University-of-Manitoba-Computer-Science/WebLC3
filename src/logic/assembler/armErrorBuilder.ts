2d3d4ca92fc97f3f4f686af88950f194f9e5af4a
Alex Kitt <alex.kitt@outlook.com>
2023-05-04 02:12:39 -0500

    Make ARM error handler support instructions that take a variable number of operands

--- src/logic/assembler/armErrorBuilder.ts
+++ src/logic/assembler/armErrorBuilder.ts
@@ -18,19 +18,39 @@ export default class ArmErrorBuilder extends ErrorBuilder
     public override operandCount(lineNum: number, tokens: string[]): string
     {
         let expected: string;
-        if (tokens[0] == ".blkw")
+
+        switch (tokens[0])
         {
-            expected = "1 or 2";
+            case ".blkw":
+                expected = "1 or 2";
+                break;
+            case "add":
+            case "asr":
+            case "lsl":
+            case "lsr":
+            case "str":
+            case "sub":
+                expected = "2 or 3";
+                break;
+            case "ldmia":
+            case "pop":
+            case "push":
+            case "stmia":
+                expected = "at least 1";
+                break;
+            default:
+                if (!ARMAssembler.validMnemonic(tokens[0]))
+                {
+                    return this.formatMessage(lineNum, "Assembler error: attempted to throw operand count error for unknown instruction/directive " + tokens[0]);
+                }
+                else
+                {
+                    //@ts-ignore
+                    expected = ARMAssembler.operandCounts.get(tokens[0]);
+                }
+                break;
         }
-        else if (!ARMAssembler.validMnemonic(tokens[0]))
-        {
-            return this.formatMessage(lineNum, "Assembler error: attempted to throw operand count error for unknown instruction/directive " + tokens[0]);
-        }
-        else
-        {
-            //@ts-ignore
-            expected = ARMAssembler.operandCounts.get(tokens[0]);
-        }
+
         return this.formatMessage(lineNum,
             "Incorrect number of operands for " + tokens[0] + ": expected " +
             expected + ", found " + (tokens.length-1));
